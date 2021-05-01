2dae3f56b7bea477e4f0b3fc990d9e4ea381d572
wokesdev <khoandy752@gmail.com>
2021-05-02 00:16:15 +0700

    Update - 2 May 2021

--- app/Http/Controllers/api/v1/DashboardController.php
+++ app/Http/Controllers/api/v1/DashboardController.php
@@ -20,4 +20,22 @@ class DashboardController extends Controller
         // Returning success API response.
         return $this->success($users, 'Users was counted sucessfully.');
     }
+
+    public function adminCount()
+    {
+        // Counting total admin.
+        $admins = User::where('is_admin', 1)->count();
+
+        // Returning success API response.
+        return $this->success($admins, 'Admins was counted sucessfully.');
+    }
+
+    public function bannedCount()
+    {
+        // Counting total banned user.
+        $banneds = User::where('is_banned', 1)->count();
+
+        // Returning success API response.
+        return $this->success($banneds, 'Banned users was counted sucessfully.');
+    }
 }
