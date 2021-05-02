0424c8f2989cb37c9ab26395a50abe38ca8816ff
wokesdev <khoandy752@gmail.com>
2021-05-02 14:00:26 +0700

    Update - 2 May 2021

--- app/Http/Controllers/api/v1/CoaController.php
+++ app/Http/Controllers/api/v1/CoaController.php
@@ -42,7 +42,7 @@ class CoaController extends Controller
     public function show(ChartOfAccount $coa)
     {
         // Getting selected chart of account.
-        $currentChartOfAccount = ChartOfAccount::with('coa_details')->get();
+        $currentChartOfAccount = ChartOfAccount::with('coa_details')->where('id', $coa->id)->get();
 
         // Returning success API response.
         return $this->success($currentChartOfAccount, 'Chart of account with that id was retrieved successfully.');
